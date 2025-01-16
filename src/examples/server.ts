edf20bcec1e0bd059959327e4110e8e7dc2ba5df
Mick <micksmits96@gmail.com>
2025-01-16 12:33:34 +0100

    log after running on port

--- src/examples/server.ts
+++ src/examples/server.ts
@@ -25,5 +25,7 @@ const app = express();
     return res.json(result);
   });
 
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
 })();
